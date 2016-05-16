f5d46a5fa4846ce837b79cdcd63a8de7483e7614
sgrebnov <sergei.grebnov@gmail.com>
2016-05-16 09:53:10 +0300

    Windows10 sample consumer app

---
+++ samples/LgAllJoynApp/LgAllJoynApp1/MainPage.xaml.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.Devices.AllJoyn;
+using org.alljoyn.Notification;
+using Windows.Foundation;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Client;
+using com.lg.Control.TV;
+using org.allseen.LSF.LampState;
+using System.Diagnostics;
+
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace LgAllJoynApp1
+{
+    public class NotificationService : INotificationService
+    {
+        public IAsyncOperation<NotificationGetVersionResult> GetVersionAsync(AllJoynMessageInfo info)
+        {
+            Task<NotificationGetVersionResult> task = new Task<NotificationGetVersionResult>(() =>
+            {
+                return NotificationGetVersionResult.CreateSuccessResult(1);
+            });
+
+            task.Start();
+            return task.AsAsyncOperation();
+        }
+    }
+
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        TVConsumer _tv;
+        LampStateConsumer _lamp;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            FindTv();
+            FindLamp();
+
+            InitializeBotCommandsProxy();
+        }
+
+        private async void ChannelUp()
+        {
+            if (_tv == null) return;
+            await _tv.UpChannelAsync();
+        }
+
+        private async void ChannelDown()
+        {
+            if (_tv == null) return;
+            await _tv.DownChannelAsync();
+        }
+
+        private void FindTv()
+        {
+            TVWatcher watcher = new TVWatcher(new AllJoynBusAttachment());
+            watcher.Added += Watcher_Added1;
+            watcher.Start();
+        }
+
+        private void FindLamp()
+        {
+            LampStateWatcher watcher = new LampStateWatcher(new AllJoynBusAttachment());
+            watcher.Added += async (sender, args) =>
+            {
+                var joinResult = await LampStateConsumer.JoinSessionAsync(args, sender);
+
+                if (joinResult.Status == AllJoynStatus.Ok)
+                {
+                    this._lamp = joinResult.Consumer;
+                    //LampOff();
+                };
+            };
+
+            watcher.Start();
+
+        }
+
+        private async void LampOff()
+        {
+            if (_lamp == null) return;
+
+            await this._lamp.SetBrightnessAsync(0);
+        }
+
+        private async void Watcher_Added1(TVWatcher sender, AllJoynServiceInfo args)
+        {
+            var joinResult = await TVConsumer.JoinSessionAsync(args, sender);
+
+            if (joinResult.Status == AllJoynStatus.Ok)
+            {
+                this._tv = joinResult.Consumer;
+            };
+        }
+
+        private void ChannelUpClick(object sender, RoutedEventArgs e)
+        {
+            ChannelUp();
+        }
+
+        private void ChannelDownClick(object sender, RoutedEventArgs e)
+        {
+            ChannelDown();
+        }
+
+        private async void InitializeBotCommandsProxy()
+        {
+            var connection = new HubConnection("https://msdevcon2016-bot.azurewebsites.net/signalr");
+
+            var hubProxy = connection.CreateHubProxy("BotConnection");
+            hubProxy.On<string>("Send", (arg) =>
+            {
+
+                switch (arg)
+                {
+                    case BotCommands.TvChannelUp:
+                        ChannelUp();
+                        break;
+                    case BotCommands.TvChannelDown:
+                        ChannelDown();
+                        break;
+                    case BotCommands.TurnLampOff:
+                        LampOff();
+                        break;
+                    default:
+                        Debug.WriteLine("Unknown command received: " + arg);
+                        break;
+                }
+            });
+
+            await connection.Start();
+        }
+    }
+
+    public class BotCommands
+    {
+        public const string TurnLampOn = "lamp_on";
+        public const string TurnLampOff = "lamp_off";
+        public const string TvChannelDown = "tv_channel_down";
+        public const string TvChannelUp = "tv_channel_up";
+    }
+}
---
+++ samples/LgAllJoynApp/LgAllJoynApp1/Notifications.cs
@@ -0,0 +1,76 @@
+﻿using org.alljoyn.Notification;
+using System;
+using System.Collections.Generic;
+using Windows.Devices.AllJoyn;
+
+namespace LgAllJoynApp1
+{
+    class Notifications
+    {
+        private static NotificationProducer _producer;
+        private static int id = 0;
+
+        public static void Send(string message) {
+
+            if (_producer == null) {
+
+                AllJoynBusAttachment busAttachment = new AllJoynBusAttachment();
+
+                _producer = new NotificationProducer(busAttachment);
+                _producer.Service = new NotificationService();
+                _producer.Start();
+            }
+
+            NotificationLangTextItem item = new NotificationLangTextItem()
+            {
+                Value1 = "en",
+                Value2 = message
+            };
+
+            List<NotificationLangTextItem> msg = new List<NotificationLangTextItem>();
+            msg.Add(item);
+
+            IReadOnlyList<byte> appId = new List<byte> { 0xE1, 0x31, 0x48, 0x15, 0x75, 0x04, 0x59, 0xC9, 0x57, 0x0D, 0x1A, 0x8E, 0xD5, 0x56, 0xDC, 0xE2 };
+
+            _producer.Signals.Notify(2, id++, 2, "v-segreb", "AllJoynTVSample", appId,
+                "AllJoynTVSample ALL", new Dictionary<int, object>(),
+                new Dictionary<String, String>(), msg);
+
+        }
+    }
+}
+
+//app.bus.sendSignal(app.getSuccessFor('sendNotification'), app.getFailureFor('sendNotification'), indexList, "qiqssaysa{iv}a{ss}a(ss)", [
+//2, // q version,
+//msgId, // i msgId (supposed to be assigned by notification service framework)
+//2, // q Type - 0 = emergency 1 - warning 2 - info
+//"1234567890", // s device id4564545455454
+//"AllJoynTVSample", // s device name
+//[123, 123, 123, 123, 123, 125, 12, 12, 123, 12, 123, 12, 12, 12, 12, 12], //app id
+//"AllJoynTVSample ALL", // s app name
+//[], // a{iv}attributes
+//[], // a{ss} custom attributes
+//[
+//    ["en", text],
+//], // a(ss) language/text
+
+//    private async void Button_Click(object sender, RoutedEventArgs e)
+//    {
+//        // string payload = textBox.Text;
+
+//        // string appId = "com.akvelon.app";// "com.webos.app.browser"
+
+//        // await _launch.StopAppAsync(appId);
+
+
+//        //var result = await _launch.StartAppAsync(appId,
+//        //    Encoding.UTF8.GetBytes(payload),
+//        //    new LaunchOptions()
+//        //    {
+//        //        Value1 = true,
+//        //        Value2 = true
+//        //    });
+//    }
+//}
+
+
